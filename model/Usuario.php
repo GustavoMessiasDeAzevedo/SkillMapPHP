4bb9dc3d072b63f00f0897df3a2da50d063507c9
GustavoMessiasDeAzevedo <134647938+GustavoMessiasDeAzevedo@users.noreply.github.com>
2026-04-23 19:31:24 -0300

    Refactor Usuario class by cleaning comments
    Removed comments and adjusted method visibility for clarity.

--- model/Usuario.php
+++ model/Usuario.php
@@ -1,13 +1,11 @@
 <?php
 
 class Usuario {
-    // Apenas declaramos o que precisa de lógica específica aqui fora
+
     private string $senha = ''; 
     private ?string $habilidadesTexto = null;
     private ?int $id = null;
 
-    // CONSTRUTOR "MÁGICO" (Property Promotion)
-    // Ao colocar 'public' ou 'private' aqui dentro, o PHP já cria a propriedade na classe
     public function __construct(
         public string $nome = '',
         public string $email = '',
@@ -15,7 +13,6 @@ class Usuario {
         
     ) {}
 
-    // --- MÉTODOS DE LÓGICA (Mantemos igual) ---
 
     public function definirSenha(string $senhaPura): void {
         $this->senha = password_hash($senhaPura, PASSWORD_DEFAULT);
@@ -29,11 +26,6 @@ class Usuario {
         return password_verify($senhaPura, $this->senha);
     }
 
-    // --- ACESSO MANUAL ---
-    // Como $nome, $email e $localizacao são PUBLIC, você não precisa de Get/Set pra eles.
-    // Você acessa direto: $usuario->nome = "Fulano";
-    
-    // Precisamos de getters manuais apenas para os PRIVADOS:
     
     public function getId(): ?int { return $this->id; }
     public function setId(int $id): void { $this->id = $id; }
